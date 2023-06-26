b154c80b63c206235f4b1abb0a4578dd050a284f
Farisbit19003 <farisirfanbit19.03@gmail.com>
2023-06-26 13:19:28 +0500

    RESPONSES

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { AiOutlineMail, AiOutlinePhone, AiOutlineToTop} from "react-icons/ai";
 const Footer = () => {
     const handleScrollToTop = () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -10,22 +10,20 @@ const Footer = () => {
         <div className="flex flex-row justify-between ">
           <div className="flex flex-col gap-1">
             <p>©2023 Saeed Azeem</p>
-            <p className="text-sm">
-              {" "}
+            <p>
               Designed by Salman Aslam &<br /> Developed by Faris irfan
             </p>
           </div>
-          <div className="flex-col md:flex-row">
-            <div className="flex flex-col md:flex-row md:p-3 p-2 md:gap-5 gap-3 sm:text-sm justify-center text-center align-middle">
-              <p className="flex ">EMAIL</p>
-              <p className="flex ">PHONE</p>
-            </div>
-            <div
-              className="flex flex-row gap-1 p-3 text-sm md:text-base cursor-pointer whitespace-nowrap"
-              onClick={handleScrollToTop}
-            >
-              <p>BACK TO TOP</p>
-            </div>
+          <div className="flex-col text-[#f6f6f6] justify-center p-1 gap-3 cursor-pointer">
+            <p>
+              <AiOutlineMail size={25} />
+            </p>
+            <p>
+              <AiOutlinePhone size={25} />
+            </p>
+            <p  onClick={handleScrollToTop}>
+              <AiOutlineToTop size={25} />
+            </p>
           </div>
         </div>
       </div>
