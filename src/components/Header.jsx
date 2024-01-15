6915872f63deecc2b1f87f9af627d39ced736671
Farisbit19003 <farisirfanbit19.03@gmail.com>
2024-01-15 17:04:24 +0500

    Your commit message here

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => {
     <>
       <div className="bg-[#161616] flex p-4 justify-between text-center">
         <p className="text-[#f6f6f6] font-aktura text-5xl">SA</p>
-        <p className="text-[#f6f6f6] font-clash text-3xl cursor-pointer transition-transform hover:scale-95">
+        <menu className="text-[#f6f6f6] font-clash text-3xl cursor-pointer transition-transform hover:scale-95">
           Menu
-        </p>
+        </menu>
       </div>
     </>
   );
