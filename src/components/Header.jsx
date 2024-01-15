93576325bf7df271c1befb162312a6ba23644043
Farisbit19003 <farisirfanbit19.03@gmail.com>
2024-01-15 19:23:05 +0500

    Your commit message here

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,29 +7,9 @@ const Header = () => {
         <p className="text-[#f6f6f6] font-aktura text-5xl">SA</p>
         <div className="flex items-center">
           <div className="flex text-[#f6f6f6] space-x-4">
-            <MenuItem title="Haircuts">
-              <SubMenuItem title="Classic Haircut" />
-              <SubMenuItem title="Modern Styles" />
-              <SubMenuItem title="Beard Trimming" />
-            </MenuItem>
-            <MenuItem title="Coloring">
-              <SubMenuItem title="Full Color" />
-              <SubMenuItem title="Highlights" />
-              <SubMenuItem title="Gray Blending" />
-            </MenuItem>
-            <MenuItem title="Facial">
-              <SubMenuItem title="Deep Cleanse" />
-              <SubMenuItem title="Anti-Aging" />
-              <SubMenuItem title="Moisturizing" />
-            </MenuItem>
-            <MenuItem title="Packages">
-              <SubMenuItem title="Grooming Package" />
-              <SubMenuItem title="VIP Package" />
-            </MenuItem>
-            <MenuItem title="Contact Us">
-              <SubMenuItem title="Email" />
-              <SubMenuItem title="Mobile" />
-            </MenuItem>
+            <MenuItem title="HOME" subMenuItems={['The Vice Chancellor', 'The Director', 'Mission Statement', 'Establishment', 'Academic Regulations', 'Overview of the University']} />
+            <MenuItem title="DEPARTMENTS" subMenuItems={['Administrative Sciences', 'Result – BSM', 'Business Administration', 'BBA Course Roadmap', 'Result – BBA', 'Commerce', 'Bcom Course Roadmap', 'Result – Bcom', 'Information Technology', 'Overview', 'CS Course Roadmap', 'Course Outline', 'Project Office', 'Result – IT Department', 'Law', 'Result – LLb', 'Code of Conduct', 'Department of English', 'Teachers of All Departments']} />
+            {/* Add other menu items here */}
           </div>
         </div>
       </div>
@@ -37,19 +17,19 @@ const Header = () => {
   );
 };
 
-const MenuItem = ({ title, children }) => {
+const MenuItem = ({ title, subMenuItems }) => {
   return (
     <div className="group relative">
       <ul>
-        {" "}
         <p className="cursor-pointer font-aktura text-2xl py-2 px-4 block hover:bg-[#333]">
           {title}
         </p>
         <li>
-          {" "}
-          {children && (
+          {subMenuItems && (
             <div className="absolute hidden space-y-2 mt-2 bg-[#161616] text-[#f6f6f6] rounded-md shadow-lg group-hover:block">
-              {children}
+              {subMenuItems.map((item, index) => (
+                <SubMenuItem key={index} title={item} />
+              ))}
             </div>
           )}
         </li>
