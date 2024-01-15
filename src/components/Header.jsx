99ee383d11bcaf69cb3bc115d562df9ec5157919
Farisbit19003 <farisirfanbit19.03@gmail.com>
2024-01-15 21:59:05 +0500

    Your commit message here

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,47 +4,52 @@ const Header = () => {
   const menuData = [
     {
       title: "Haircuts",
+      link: "#",
       children: [
-        { title: "Classic Haircut" },
-        { title: "Modern Styles" },
-        { title: "Beard Trimming" },
+        { title: "Classic Haircut", link: "#" },
+        { title: "Modern Styles", link: "#" },
+        { title: "Beard Trimming", link: "#" },
       ],
     },
     {
       title: "Coloring",
+      link: "#",
       children: [
-        { title: "Full Color" },
-        { title: "Highlights" },
-        { title: "Gray Blending" },
+        { title: "Full Color", link: "#" },
+        { title: "Highlights", link: "#" },
+        { title: "Gray Blending", link: "#" },
       ],
     },
     {
       title: "Facial",
+      link: "#",
       children: [
-        { title: "Deep Cleanse" },
-        { title: "Anti-Aging" },
-        { title: "Moisturizing" },
+        { title: "Deep Cleanse", link: "#" },
+        { title: "Anti-Aging", link: "#" },
+        { title: "Moisturizing", link: "#" },
       ],
     },
     {
       title: "Packages",
+      link: "#",
       children: [
-        { title: "Grooming Package" },
-        { title: "VIP Package" },
+        { title: "Grooming Package", link: "#" },
+        { title: "VIP Package", link: "#" },
       ],
     },
     {
       title: "Contact Us",
+      link: "#",
       children: [
-        { title: "Email" },
-        { title: "Mobile" },
+        { title: "Email", link: "#" },
+        { title: "Mobile", link: "#" },
       ],
     },
   ];
 
   const renderMenuItems = (items) => {
     return items.map((item, index) => (
-      <MenuItem key={index} title={item.title}>
+      <MenuItem key={index} title={item.title} link={item.link}>
         {item.children && renderMenuItems(item.children)}
       </MenuItem>
     ));
@@ -53,7 +58,9 @@ const Header = () => {
   return (
     <>
       <div className="bg-[#161616] flex p-4 justify-between text-center">
-        <a href="#" className="text-[#f6f6f6] font-aktura text-5xl">SA</a>
+        <a href="#" className="text-[#f6f6f6] font-aktura text-5xl">
+          SA
+        </a>
         <div className="flex items-center">
           <div className="flex text-[#f6f6f6] space-x-4">
             {renderMenuItems(menuData)}
@@ -64,11 +71,14 @@ const Header = () => {
   );
 };
 
-const MenuItem = ({ title, children }) => {
+const MenuItem = ({ title, link, children }) => {
   return (
     <div className="group relative">
       <ul>
-        <a href="#" className="cursor-pointer  text-2xl py-2 px-4 block hover:bg-[#333]">
+        <a
+          href={link}
+          className="cursor-pointer text-2xl py-2 px-4 block hover:bg-[#333]"
+        >
           {title}
         </a>
         <li>
